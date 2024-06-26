114c49d7c61bdec250b747bf7128b9b4128775aa
quocvan1999 <33951682+quocvan1999@users.noreply.github.com>
2024-06-26 22:53:50 +0700

    update

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,21 +1,9 @@
-document.querySelector("#iconClose").onclick = function () {
-  let arrIconClose = document.querySelectorAll("#iconClose i");
-  let menuContainer = document.querySelector("#menu");
-  let arrTextMenu = document.querySelectorAll("#menu__list li a");
-  let footerContainer = document.querySelector("footer");
-  let logoText = document.querySelector("#logo__container div span");
+document.querySelector("#iconClose1").onclick = function () {
+  document.querySelector("#menu").classList.toggle("menu-active");
+};
 
-  for (const text of arrTextMenu) {
-    text.classList.toggle("d-none");
-  }
-
-  for (const iconClose of arrIconClose) {
-    iconClose.classList.toggle("d-none");
-  }
-
-  footerContainer.classList.toggle("d-none");
-  logoText.classList.toggle("d-none");
-  menuContainer.classList.toggle("menu-active");
+document.querySelector("#iconClose2").onclick = function () {
+  document.querySelector("#menu").classList.toggle("d-flex");
 };
 
 document.querySelector("#iconThongBao").onclick = function () {
@@ -30,4 +18,8 @@ document.querySelector("#btnMode").onclick = function () {
   document.querySelector("body").classList.toggle("dark-mode");
   document.querySelector("#btnMode i:first-child").classList.toggle("d-none");
   document.querySelector("#btnMode i:last-child").classList.toggle("d-none");
+};
+
+document.querySelector("#btnButtonMenu").onclick = function () {
+  document.querySelector("#menu").classList.toggle("d-flex");
 };
