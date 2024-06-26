7cdfc223d217e8dc07434b8d29249f52a0bf5127
quocvan1999 <33951682+quocvan1999@users.noreply.github.com>
2024-06-26 21:07:56 +0700

    update

---
+++ admin/model/Product.js
@@ -0,0 +1,10 @@
+export class Product {
+  name = "";
+  price = 0;
+  screen = "";
+  backCamera = "";
+  frontCamera = "";
+  img = "";
+  desc = "";
+  type = "";
+}
--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,11 +1,10 @@
-let iconClose = document.querySelector("#iconClose");
-let arrIconClose = document.querySelectorAll("#iconClose i");
-let menuContainer = document.querySelector("#menu");
-let arrTextMenu = document.querySelectorAll("#menu__list li a");
-let footerContainer = document.querySelector("footer");
-let logoText = document.querySelector("#logo__container div span");
+document.querySelector("#iconClose").onclick = function () {
+  let arrIconClose = document.querySelectorAll("#iconClose i");
+  let menuContainer = document.querySelector("#menu");
+  let arrTextMenu = document.querySelectorAll("#menu__list li a");
+  let footerContainer = document.querySelector("footer");
+  let logoText = document.querySelector("#logo__container div span");
 
-iconClose.addEventListener("click", () => {
   for (const text of arrTextMenu) {
     text.classList.toggle("d-none");
   }
@@ -17,4 +16,18 @@ iconClose.addEventListener("click", () => {
   footerContainer.classList.toggle("d-none");
   logoText.classList.toggle("d-none");
   menuContainer.classList.toggle("menu-active");
-});
+};
+
+document.querySelector("#iconThongBao").onclick = function () {
+  document.querySelector("#showThongBao").classList.toggle("d-none");
+};
+
+document.querySelector("#dongThongBao").onclick = function () {
+  document.querySelector("#showThongBao").classList.toggle("d-none");
+};
+
+document.querySelector("#btnMode").onclick = function () {
+  document.querySelector("body").classList.toggle("dark-mode");
+  document.querySelector("#btnMode i:first-child").classList.toggle("d-none");
+  document.querySelector("#btnMode i:last-child").classList.toggle("d-none");
+};
